4e7ed61229ffb6a7d4c58e94f757cad4595ee8b9
gamoradima <gamoradima@gmail.com>
2024-06-19 11:51:25 +0300

    SQL and data items examples

---
+++ DevClassic/SqlScripts/UsrDelLandRealtyTypeSQL/UsrDelLandRealtyTypeSQL.sql
@@ -0,0 +1,4 @@
+delete from UsrRealtyTypeClassic
+where Id = '2ef4ffd5-f953-4e99-bdc3-5d6e9949302e'
+
+-- example for PostgreSQL: delete from public."UsrRealtyTypeClassic" where "Id" = '2ef4ffd5-f953-4e99-bdc3-5d6e9949302e'
