39c958bd04ea74d24da55772e4c3a0e10a008bee
gamoradima <gamoradima@gmail.com>
2024-06-25 17:07:12 +0300

    Precofigured page example

---
+++ UsrRealty/Schemas/UsrCalcAveragePriceResultPage/UsrCalcAveragePriceResultPage.js
@@ -0,0 +1,104 @@
+define("UsrCalcAveragePriceResultPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "CancelButton",
+				"values": {
+					"color": "default",
+					"size": "large",
+					"iconPosition": "only-text"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "Tabs",
+				"values": {
+					"styleType": "default",
+					"mode": "tab",
+					"bodyBackgroundColor": "primary-contrast-500",
+					"selectedTabTitleColor": "auto",
+					"tabTitleColor": "auto",
+					"underlineSelectedTabColor": "auto",
+					"headerBackgroundColor": "auto"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "NumberInput_w8l5hba",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PageParameters_UsrIntegerParameter1_hgw55t3",
+					"labelPosition": "auto",
+					"control": "$PageParameters_UsrIntegerParameter1_hgw55t3",
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "GridContainer_uxln7d4",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "RichTextEditor_h5x9gc7",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 4
+					},
+					"type": "crt.RichTextEditor",
+					"label": "$Resources.Strings.PageParameters_UsrRichTextParameter1_jcrrcs0",
+					"labelPosition": "auto",
+					"control": "$PageParameters_UsrRichTextParameter1_jcrrcs0",
+					"filesStorage": {
+						"masterRecordColumnValue": "$Id",
+						"entitySchemaName": "SysFile",
+						"recordColumnName": "RecordId"
+					},
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": "",
+					"toolbarDisplayMode": null
+				},
+				"parentName": "GridContainer_uxln7d4",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrIntegerParameter1_hgw55t3": {
+						"modelConfig": {
+							"path": "PageParameters.UsrAvgPriceParameter"
+						}
+					},
+					"PageParameters_UsrRichTextParameter1_jcrrcs0": {
+						"modelConfig": {
+							"path": "PageParameters.UsrSQLTextParameter"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
